ab4bbe4ef203621c50066fdb6c2bb5d273e27c5b
Richard Coll <richarddcoll@gmail.com>
2025-09-29 17:48:03 +0100

    need to set the csp on headers

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Credentials", true);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  res.setHeader("Content-Security-Policy", "script-src 'self' https://apis.google.com");
   next();
 });
 

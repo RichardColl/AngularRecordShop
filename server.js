e0d7d3827480c5d2bb81f99a2a75aec10aabab83
Richard Coll <richarddcoll@gmail.com>
2025-09-29 18:11:18 +0100

    need to set the csp on headers with default src to self

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Credentials", true);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-  res.setHeader("Content-Security-Policy", "default-src 'self' script-src 'self' https://apis.google.com");
+  res.setHeader("Content-Security-Policy", 'default-src 'self' script-src 'self' https://apis.google.com');
   next();
 });
 

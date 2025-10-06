4f78adda136ef5b47ed22ce299a6d4f6444f46ef
Richard Coll <richarddcoll@gmail.com>
2025-10-06 16:06:38 +0100

    adding control headers

--- index.js
+++ index.js
@@ -15,6 +15,14 @@ app.get('/*', function (req, res) {
     res.sendFile(distDir);
 });
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
 
 const server = app.listen(port, () => {
   console.log(`Listening on ${port}`)

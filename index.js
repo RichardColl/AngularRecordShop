946a3ac2760c5447c81e2f9e489f59e4b5434867
Richard Coll <richarddcoll@gmail.com>
2025-10-08 12:16:35 +0100

    try link index js express to angular dist folder

--- index.js
+++ index.js
@@ -5,14 +5,11 @@ const port = process.env.PORT || 5006
 
 const app = express()
 
-app.use(express.static(path.join(__dirname, 'public')))
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  console.log(`Rendering 'pages/index' for route '/'`)
-  res.render('pages/index')
-})
+// Create link to Angular build directory
+var distDir = __dirname + "/dist/";
+app.use(express.static(distDir));
 
 const server = app.listen(port, () => {
   console.log(`Listening on ${port}`)

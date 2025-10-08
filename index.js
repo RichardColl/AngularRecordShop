6314f3b6772c7ffeb34197648f79f0ff9fbb32fc
Richard Coll <richarddcoll@gmail.com>
2025-10-08 11:53:23 +0100

    adding procfile

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(function(req, res, next) {
 
 
  // Initialize the app.
-  var server = app.listen(process.env.PORT || 8080, function () {
+  var server = app.listen(process.env.PORT || 3000, function () {
     var port = server.address().port;
     console.log("App now running on port", port);
   });

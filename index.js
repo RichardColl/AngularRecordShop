b746d419b919f5ff45b1c1eeeefe64b590ac348c
Richard Coll <richarddcoll@gmail.com>
2025-10-09 12:12:59 +0100

    new run commands

--- index.js
+++ index.js
@@ -1,54 +1,22 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-//var mongodb = require("mongodb");
-//var ObjectID = mongodb.ObjectID;
+const express = require('express')
+const path = require('path')
 
-var CONTACTS_COLLECTION = "contacts";
+const port = process.env.PORT || 5006
 
-var app = express();
-app.use(bodyParser.json());
+const app = express()
 
-// Create link to Angular build directory
-var distDir = __dirname + "/dist/";
-app.use(express.static(distDir));
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
-app.get('/*', function (req, res) {
-    res.sendFile(distDir);
-});
+app.get('/', (req, res) => {
+  console.log(`Rendering 'pages/index' for route '/'`)
+  res.render('pages/index')
+})
 
-
-
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-  next();
-});
-
-// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
-var db;
-
-// Connect to the database before starting the application server.
-//mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", function (err, client) {
-//  if (err) {
-//    console.log(err);
-//    process.exit(1);
-//  }
-
-  // Save database object from the callback for reuse.
-//  db = client.db();
-//  console.log("Database connection ready");
-
-  // Initialize the app.
-  var server = app.listen(process.env.PORT || 8080, function () {
-    var port = server.address().port;
-    console.log("App now running on port", port);
-  });
-
-
-
+const server = app.listen(port, () => {
+  console.log(`Listening on ${port}`)
+})
 
 // The number of seconds an idle Keep-Alive connection is kept open. This should be greater than the Heroku Router's
 // Keep-Alive idle timeout of 90 seconds:
